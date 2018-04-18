5ec85ca71d96b79ee536b56a1b8de6e6e037f6e8
valeria campilongo <cvaleriac@hotmail.it>
2018-04-18 17:30:45 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,6 @@
 def my_collect(language)
     lang = 0
     collection = []
-    my_collect(collection) do |lang|
-  lang.upcase
-end
     while lang < language.length
       collection << yield(language[lang])
       lang += 1
